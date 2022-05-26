fb2ee39040384a41ffc7b682f2f62c368edcff84
Abdelrahman Gamal <33043163+AbdelrahmanGamal-jimy2@users.noreply.github.com>
2022-05-27 01:40:56 +0200

    finalized my project structure and added images to the assets folder

--- build/index.js
+++ build/index.js
@@ -1,2 +1,3 @@
-"use strict";
-console.log('testing');
+'use strict';
+var mano = 0;
+console.log(mano);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('testing');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/api', (req, res) =>{
+    res.send('Welcome to Image Processing API :)');
+});
+
+app.listen(port, () => { 
+    console.log(`server started at localhost ${port}`);
+});
